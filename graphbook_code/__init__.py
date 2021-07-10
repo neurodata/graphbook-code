432408849381c39ae6975c2aab7e4383bddd1deb
loftusa <alexloftus2004@gmail.com>
2021-07-10 15:24:31 -0400

    add top-level import

--- graphbook_code/__init__.py
+++ graphbook_code/__init__.py
@@ -3,5 +3,8 @@
 """Top-level package for graphbook-code."""
 
 __author__ = """Alex Loftus"""
-__email__ = 'aloftus2@jhu.edu'
-__version__ = '0.1.0'
+__email__ = "aloftus2@jhu.edu"
+__version__ = "0.1.0"
+
+__all__ = ["binary_heatmap"]
+from .main import *
