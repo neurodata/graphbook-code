856b4b64adeaa5459be8bdb376292f18d44513f2
loftusa <alexloftus2004@gmail.com>
2021-10-11 16:11:05 -0400

    add function to draw cartesian coordinate axis

--- graphbook_code/plotting.py
+++ graphbook_code/plotting.py
@@ -159,6 +159,68 @@ def plot_network(network, labels, color="sequential", *args, **kwargs):
     heatmap(network, labels, color=color, *args, **kwargs)
 
 
+def draw_cartesian(xrange=(-2, 2), yrange=(-2, 2), ticks_frequency=5, ax=None):
+    """
+    Draw a cartesian coordinate axis.
+
+    Parameters
+    ----------
+    xrange : tuple, optional
+        xmin, xmax, by default (-2, 2)
+    yrange : tuple, optional
+        ymin, ymax, by default (-2, 2)
+    ticks_frequency : int, optional
+        interval for ticks, by default 5
+    ax : [type], optional
+        if None, makes new fig and ax, by default None
+
+    Returns
+    -------
+    ax
+        Cartesian coordinate axis.
+    """
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(5, 5))
+    xmin, xmax = xrange
+    ymin, ymax = yrange
+
+    # Set identical scales for both axes
+    ax.set(xlim=(xmin - 1, xmax + 1), ylim=(ymin - 1, ymax + 1), aspect="equal")
+
+    # Set bottom and left spines as x and y axes of coordinate system
+    ax.spines["bottom"].set_position("zero")
+    ax.spines["left"].set_position("zero")
+
+    # Remove top and right spines
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+
+    # Create 'x' and 'y' labels placed at the end of the axes
+    ax.set_xlabel("x", size=14, labelpad=-24, x=1.03)
+    ax.set_ylabel("y", size=14, labelpad=-21, y=1.02, rotation=0)
+
+    # Create custom major ticks to determine position of tick labels
+    x_ticks = np.arange(xmin, xmax + 1, ticks_frequency)
+    y_ticks = np.arange(ymin, ymax + 1, ticks_frequency)
+    ax.set_xticks(x_ticks[x_ticks != 0])
+    ax.set_yticks(y_ticks[y_ticks != 0])
+
+    # Create minor ticks placed at each integer to enable drawing of minor grid
+    # lines: note that this has no effect in this example with ticks_frequency=1
+    ax.set_xticks(np.arange(xmin, xmax + 1), minor=True)
+    ax.set_yticks(np.arange(ymin, ymax + 1), minor=True)
+
+    # Draw major and minor grid lines
+    ax.grid(which="both", color="grey", linewidth=1, linestyle="-", alpha=0.05)
+
+    # Draw arrows
+    arrow_fmt = dict(markersize=4, color="black", clip_on=False)
+    ax.plot((1), (0), marker=">", transform=ax.get_yaxis_transform(), **arrow_fmt)
+    ax.plot((0), (1), marker="^", transform=ax.get_xaxis_transform(), **arrow_fmt)
+
+    return ax
+
+
 def plot_latents(
     latent_positions,
     *,
