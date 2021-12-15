93d961e8bbcf3208010b8515dab197fdf1ede5a2
Eric W. Bridgeford <ebridge2@jhu.edu>
2021-12-14 22:43:18 -0500

    updated reqs for statsmodels

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.rst") as readme_file:
 with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = ["graspologic", "seaborn", "matplotlib"]
+requirements = ["graspologic", "seaborn", "matplotlib", "statsmodels"]
 
 setup_requirements = [
     "pytest-runner",
