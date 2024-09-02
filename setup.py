98c610fe7ea306fd00377c5fb47a8b6497ab3134
Eric W. Bridgeford <ericwb95@gmail.com>
2024-09-01 21:29:34 -0700

    updated

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
 requirements = ["graspologic", "seaborn", "matplotlib", "statsmodels", 'hyppo', 'numpy>=1.8.0', 
-                'FisherExact @ git+https://github.com/ebridge2/FisherExact@master', 'typing']
+                'typing']
 
 setup_requirements = [
     "pytest-runner",
